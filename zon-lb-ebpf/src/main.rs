e5589beb487b3fafe048dbbb1c32ce6066a995df
Catalin Horghidan <catalin.horghidan@gmail.com>
2023-12-18 20:04:14 +0200

    zon-lb-ebpf: add test map

--- zon-lb-ebpf/src/main.rs
+++ zon-lb-ebpf/src/main.rs
@@ -1,8 +1,37 @@
 #![no_std]
 #![no_main]
 
-use aya_bpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
+use aya_bpf::{
+    bindings::xdp_action,
+    macros::{map, xdp},
+    maps::HashMap,
+    programs::XdpContext,
+};
 use aya_log_ebpf::info;
+use core::mem;
+use network_types::{
+    eth::{EthHdr, EtherType},
+    icmp::IcmpHdr,
+    ip::{IpProto, Ipv4Hdr},
+    tcp::TcpHdr,
+    udp::UdpHdr,
+};
+
+#[inline(always)]
+fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
+    }
+
+    Ok((start + offset) as *const T)
+}
+
+#[map]
+static BLOCKLIST: HashMap<u32, u32> = HashMap::<u32, u32>::pinned(5, 0);
 
 #[xdp]
 pub fn zon_lb(ctx: XdpContext) -> u32 {
@@ -12,8 +41,25 @@ pub fn zon_lb(ctx: XdpContext) -> u32 {
     }
 }
 
-fn try_zon_lb(ctx: XdpContext) -> Result<u32, u32> {
+fn try_zon_lb(ctx: XdpContext) -> Result<u32, ()> {
     info!(&ctx, "received a packet");
+
+    let ethhdr: *const EthHdr = ptr_at(&ctx, 0)?;
+
+    match unsafe { (*ethhdr).ether_type } {
+        EtherType::Ipv4 => {
+            info!(&ctx, "received a ipv4 packet");
+        }
+        _ => {
+            info!(&ctx, "received a non-ipv4 packet");
+            return Ok(xdp_action::XDP_PASS);
+        }
+    }
+
+    let ipv4hdr: *const Ipv4Hdr = ptr_at(&ctx, EthHdr::LEN)?;
+    let src_addr = u32::from_be(unsafe { (*ipv4hdr).src_addr });
+    let dst_addr = u32::from_be(unsafe { (*ipv4hdr).dst_addr });
+
     Ok(xdp_action::XDP_PASS)
 }
 
