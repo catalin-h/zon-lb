0a379cd410339e455a722c8932a58e228709a30e
Catalin Horghidan <catalin.horghidan@gmail.com>
2024-02-12 21:54:54 +0200

    app: config: implement group writing

--- zon-lb/src/config.rs
+++ zon-lb/src/config.rs
@@ -1,6 +1,7 @@
 use crate::{
-    backends::{Backend as BCKND, Group, ToEndPoint},
+    backends::{Backend as BCKND, EndPoint, Group, ToEndPoint},
     helpers,
+    protocols::Protocol,
 };
 use anyhow::{anyhow, Context};
 use serde::{Deserialize, Serialize};
@@ -17,6 +18,16 @@ struct EP {
     ip: IpAddr,
     proto: u8,
     port: u16,
+}
+
+impl Into<EndPoint> for &EP {
+    fn into(self) -> EndPoint {
+        EndPoint {
+            ipaddr: self.ip,
+            proto: Protocol::from(self.proto),
+            port: self.port,
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -36,7 +47,6 @@ impl NetIf {
 #[derive(Serialize, Deserialize)]
 struct Config {
     netif: HashMap<String, NetIf>,
-
     backend: HashMap<String, EP>,
 }
 
@@ -48,11 +58,14 @@ impl Config {
         }
     }
 
-    pub fn description(&self) -> String {
-        let gcount = self
-            .netif
+    fn group_count(&self) -> usize {
+        self.netif
             .iter()
-            .fold(0, |acc, (_, g)| acc + g.groups.len());
+            .fold(0, |acc, (_, g)| acc + g.groups.len())
+    }
+
+    pub fn description(&self) -> String {
+        let gcount = self.group_count();
         format!(
             "Groups: {gcount} in {} netifs and {} backends",
             self.netif.len(),
@@ -88,6 +101,10 @@ impl ConfigFile {
         let cfg: Config = toml::from_str(&contents)?;
 
         log::info!("Found in config: {}", cfg.description());
+
+        let cfgw = ConfigWriter::new();
+        cfgw.write(&cfg)?;
+
         Ok(())
     }
 
@@ -150,5 +167,76 @@ impl ConfigFile {
         }
 
         Ok(cfg)
+    }
+}
+
+struct ConfigWriter {
+    mapping: HashMap<u16, u16>,
+    gcount: usize,
+    bcount: usize,
+}
+
+impl ConfigWriter {
+    fn new() -> Self {
+        Self {
+            mapping: HashMap::new(),
+            gcount: 0,
+            bcount: 0,
+        }
+    }
+
+    fn write(mut self, cfg: &Config) -> Result<(), anyhow::Error> {
+        log::info!("Writing config ...");
+
+        for (name, nif) in cfg.netif.iter() {
+            match Group::new(&name) {
+                Ok(group) => self.load_groups(group, &nif),
+                Err(e) => {
+                    log::error!("Skip group in {}, {}", name, e)
+                }
+            }
+        }
+
+        log::info!(
+            "Write summary: {}/{} groups, {}/{} backends",
+            self.gcount,
+            cfg.group_count(),
+            self.bcount,
+            cfg.backend.len()
+        );
+        Ok(())
+    }
+
+    fn load_groups(&mut self, group: Group, netif: &NetIf) {
+        for (gid, ep) in netif.groups.iter() {
+            let gid = match gid.parse::<u16>() {
+                Ok(g) => g,
+                Err(e) => {
+                    log::error!(
+                        "Invalid group id {} for netdev: {}, {}",
+                        gid,
+                        group.ifname,
+                        e
+                    );
+                    continue;
+                }
+            };
+            let ep = ep.into();
+            log::info!("Adding backend group {} to {}", ep, group.ifname);
+            let actual_gid = match group.add(&ep) {
+                Ok(id) => id,
+                Err(e) => {
+                    log::error!(
+                        "Failed to add backend group {} to {}, {}",
+                        ep,
+                        group.ifname,
+                        e
+                    );
+                    continue;
+                }
+            };
+            self.mapping.insert(gid, actual_gid as u16);
+            self.gcount += 1;
+        }
     }
 }
