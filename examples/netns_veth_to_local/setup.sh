5c27eea28d04e38d7a528d8f99fe3e1404f50cb0
Catalin Horghidan <catalin.horghidan@gmail.com>
2024-03-20 20:36:41 +0200

    test: add script for testing with veth and netns

---
+++ examples/netns_veth_to_local/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+NS=zone1
+IF0=veth0
+IF1=veth1
+IP0=10.0.0.1
+IP1=10.0.0.2
+
+if [ "$1" = "cleanup" ]; then
+	printf "cleanup ..\n"
+	# local veth will be deleted after deleting the netns
+	ip netns del $NS
+	printf "done!\n"
+	exit 0
+fi
+
+if [ ! -f /var/run/netns/$NS ]; then
+	ip netns add $NS
+else
+	printf "netns $NS and veth already created\n"
+	ip -s address show dev $IF0
+	ip -s -netns $NS address show dev $IF1
+	printf  "done!\n"
+	exit 0
+fi
+
+ip link add name $IF0 type veth peer netns $NS name $IF1
+ip link set dev $IF0 up
+ip -netns $NS link set dev $IF1 up
+# by default the loopback interface in newly created netns is down
+ip -netns $NS link set dev lo up
+ip address add $IP0/24 dev $IF0
+ip -netns $NS address add $IP1/24 dev $IF1
+ip netns exec $NS ping $IP0 -c1 -s0
+ip -s address show dev $IF0
+ip -s -netns $NS address show dev $IF1
+
+printf "done!\n"
+
