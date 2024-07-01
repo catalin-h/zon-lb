4d251bad303d793f655491333740a144c8c69f05
Catalin Horghidan <catalin.horghidan@gmail.com>
2024-07-01 20:03:23 +0300

    app: show IPv6 conntrack entries

--- zon-lb/src/conntrack.rs
+++ zon-lb/src/conntrack.rs
@@ -9,7 +9,7 @@ use anyhow::anyhow;
 use aya::maps::{HashMap, Map, MapData};
 use log::info;
 use std::net::IpAddr;
-use zon_lb_common::{NAT4Key, NAT4Value, NAT6Key};
+use zon_lb_common::{NAT4Key, NAT4Value, NAT6Key, NAT6Value};
 
 impl ToMapName for NAT4Key {
     fn map_name() -> &'static str {
@@ -36,10 +36,11 @@ where
 }
 
 pub fn conntrack_list(_gid: u32) -> Result<(), anyhow::Error> {
-    let ctm = conntrack_mapdata::<NAT4Key, NAT4Value>()?;
+    let ctm4 = conntrack_mapdata::<NAT4Key, NAT4Value>()?;
     let mut ifc = IfCache::new();
     let mut tab = InfoTable::new(vec!["proto", "src", "lb", "backend", "if", "vlan"]);
-    for (key, value) in ctm.iter().filter_map(|f| f.ok()) {
+
+    for (key, value) in ctm4.iter().filter_map(|f| f.ok()) {
         let src = EndPoint {
             ipaddr: IpAddr::from(value.ip_src.to_le_bytes()),
             port: u16::from_be(key.port_lb_dst),
@@ -68,7 +69,38 @@ pub fn conntrack_list(_gid: u32) -> Result<(), anyhow::Error> {
         ]);
     }
 
-    tab.print("Connection tracking and NAT IPv4 table");
+    let ctm6 = conntrack_mapdata::<NAT6Key, NAT6Value>()?;
+
+    for (key, value) in ctm6.iter().filter_map(|f| f.ok()) {
+        let src = EndPoint {
+            ipaddr: IpAddr::from(unsafe { value.ip_src.addr8 }),
+            port: u16::from_be(key.port_lb_dst as u16) as u16,
+            proto: Protocol::None,
+            ..Default::default()
+        };
+        let lb = EndPoint {
+            ipaddr: IpAddr::from(unsafe { key.ip_lb_dst.addr8 }),
+            port: u16::from_be(value.port_lb as u16),
+            proto: Protocol::None,
+            ..Default::default()
+        };
+        let be = EndPoint {
+            ipaddr: IpAddr::from(unsafe { key.ip_be_src.addr8 }),
+            port: u16::from_be(key.port_be_src as u16),
+            proto: Protocol::None,
+            ..Default::default()
+        };
+        tab.push_row(vec![
+            format!("{:?}", Protocol::from(key.next_hdr as u8)),
+            src.to_string(),
+            lb.to_string(),
+            be.to_string(),
+            format!("{}:{}", ifc.name(value.ifindex, "na"), value.ifindex),
+            format!("{:x}", value.vlan_hdr.to_be() & 0xFFF),
+        ]);
+    }
+
+    tab.print("Connection tracking");
     Ok(())
 }
 
